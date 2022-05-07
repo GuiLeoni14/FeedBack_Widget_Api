95c37dc73f530280cc18d41a3f1c0f7d013aa949
GuiLeoni14 <guileone167@gmail.com>
2022-05-06 21:00:20 -0300

    configurando port

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('SERVER ON!'));
+app.listen(process.env.PORT || 3333, () => console.log('SERVER ON!'));
